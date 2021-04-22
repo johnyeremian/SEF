3e6cbe76e9b2afbb777fb16c0cd3e11cf2b6d780
John Yeremian <johnyeremian12@gmail.com>
2021-04-22 14:19:55 -0400

    Created Classifier class with data member

---
+++ classifier.cpp
@@ -0,0 +1,11 @@
+// Filename: classifier.cpp
+// By: John Yeremian
+// Description: A basic .cpp file for Assignment 5 
+// Date: April 22, 2021
+
+#include <stdio.h>
+
+class Classifier
+{
+	int data = 0;
+};
